06ebf1530a802a1489292a0268d0a79bec3119a9
Ushinji <siguma971@gmail.com>
2019-05-12 01:34:57 +0900

    Add user model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import Model, SQLAlchemy
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, DateTime
+from datetime import datetime
 from app import application
 
 
@@ -9,6 +10,10 @@ class RecordNotFoundError(Exception):
 
 class BaseModel(Model):
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, nullable=False)
 
 
 db = SQLAlchemy(application, model_class=BaseModel)
+
+from .user import User  # nopep8
